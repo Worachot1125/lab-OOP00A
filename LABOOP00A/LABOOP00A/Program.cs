3b08eca7ad20dcf949a6a0294a4552f4a54902a1
Worachot-6534502990 <119285571+Worachot-6534502990@users.noreply.github.com>
2022-11-28 15:34:24 +0700

    add name to program

--- LABOOP00A/LABOOP00A/Program.cs
+++ LABOOP00A/LABOOP00A/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // print string to commanline
 Console.WriteLine("Worachot Thonglert");
-
+// id
+Console.WriteLine("653450299-0");
+// email
+Console.WriteLine("worachot.t@kkumail.com");
